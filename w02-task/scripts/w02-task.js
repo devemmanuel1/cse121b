583393f8fe2bbdf5b4fb7efacd68c9d8ed051c02
dev_Emmanuel <daxzibit1@>
2024-03-13 17:06:37 +0100

    changes

--- w02-task/scripts/w02-task.js
+++ w02-task/scripts/w02-task.js
@@ -11,8 +11,8 @@ let profilePicture = "images/profile.jpg";
 /* Step 3 - Element Variables */
 const nameElement = document.getElementById("name");
 const foodElement = document.getElementById("food");
-const yearElement = document.querySelector("#year");
-const imageElement = document.querySelector("img");
+const yearElement = document.querySelector('#year');
+const imageElement = document.querySelector('img');
 
 /* Step 4 - Adding Content */
 yearElement.textContent = currentYear;
